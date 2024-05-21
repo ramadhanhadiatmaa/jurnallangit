ad72613ee937b287219f22c551b9fd7f51d72197
kodlitecom <nandaaprilia605@gmail.com>
2024-05-21 09:06:43 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/jurnallangit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jurnallangit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/splashscreen/bindings/splashscreen_binding.dart';
+import '../modules/splashscreen/views/splashscreen_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const initial = Routes.SPLASHSCREEN;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASHSCREEN,
+      page: () => const SplashscreenView(),
+      binding: SplashscreenBinding(),
+    ),
+  ];
+}
